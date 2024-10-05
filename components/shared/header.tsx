cf3129c50e175da0f666f38903a0de8b218a35a1
Sallein <saleinkyl@gmail.com>
2024-10-05 22:44:20 +0300

    done basket`s button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,30 +1,55 @@
-import React from "react";
-import { cn } from "@/lib/utils";
-import { Container } from "./container";
-import Image from "next/image";
-import { Button } from "../ui";
+import React from "react"
+import { cn } from "@/lib/utils"
+import { Container } from "./container"
+import Image from "next/image"
+import { Button } from "../ui"
+import { ShoppingCart, User, ArrowRight } from "lucide-react"
 
 interface Props {
-    classname?: string;
+    classname?: string
 }
 
-export const Header: React.FC<Props> = ({ classname}) => {
+export const Header: React.FC<Props> = ({ classname }) => {
     return (
-        <header className={cn('border border-b', classname)}>
+        <header className={cn("border border-b", classname)}>
             <Container className="flex items-center justify-between py-8">
-
                 {/* Левая часть */}
                 <div className="flex items-center gap-4">
                     <Image src="/logo.png" alt="logo" width={32} height={32} />
                     <div>
-                        <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-                        <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                        <h1 className="text-2xl uppercase font-black">
+                            Next Pizza
+                        </h1>
+                        <p className="text-sm text-gray-400 leading-3">
+                            вкусней уже некуда
+                        </p>
                     </div>
                 </div>
 
                 {/* Правая часть */}
                 <div className="flex items-center gap-3">
-                    <Button variant="outline">Войти</Button>
+                    <Button
+                        variant="outline"
+                        className="flex items-center gap-1"
+                    >
+                        <User size={16} />
+                        Войти
+                    </Button>
+
+                    <div>
+                        <Button className="group relative">
+                            <b>30р</b>
+                            <span className="h-full w-[1px] bg-white/30 mx-3" />
+                            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                                <ShoppingCart
+                                    className="h-4 w-4 realtive"
+                                    strokeWidth={2}
+                                />
+                                <b>3</b>
+                            </div>
+                            <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+                        </Button>
+                    </div>
                 </div>
             </Container>
         </header>
