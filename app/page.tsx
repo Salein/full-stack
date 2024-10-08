f57d1dd20cc750957934e3d9f28f9af44f70b672
Sallein <saleinkyl@gmail.com>
2024-10-08 21:59:32 +0300

    topbar is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { Container, Title } from "@/components/shared";
-import { TopBar } from "@/components/shared/top-bar";
+import { Container, Title, TopBar } from "@/components/shared"
 
 export default function Home() {
-  return (
-    <>
-  <Container className="mt-10">
-    <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-    <TopBar />
-  </Container>
-    </>
-  );
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extrabold" />
+            </Container>
+            <TopBar />
+        </>
+    )
 }
